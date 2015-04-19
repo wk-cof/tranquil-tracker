74bc24e6717587da25bbfcfe211e261d91e78bec
Mikhail Gorbachev <gorbachevma@gmail.com>
2015-04-19 13:03:22 -0400

    charts still don't work

--- client/app/analyze/analyze.controller.js
+++ client/app/analyze/analyze.controller.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('frontendApp')
-  .controller('AnalyzeCtrl',  [ 'd3Service', function (d3Service) {
-    
+  .controller('AnalyzeCtrl',  [ function ($scope) {
+      	$scope.greeting = "Resize the page to see the re-rendering";
+	    $scope.data = [
+	      {name: "Greg", score: 98},
+	      {name: "Ari", score: 96},
+	      {name: 'Q', score: 75},
+	      {name: "Loser", score: 48}
+	    ];
   } ] );
